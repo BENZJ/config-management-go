f7abef8fb26421eb42074f25718f8a6bb8e10e1b
BenzJ <jbcbenz@gmail.com>
2023-12-22 09:42:22 +0800

    feat: 文件预览接口

--- controller/file_item_controller.go
+++ controller/file_item_controller.go
@@ -23,5 +23,6 @@ func (c *FileItemController) Register(app *gin.Engine) error {
 	g := app.Group("/fileItem")
 	g.POST("/create", c.fileItemService.CreateFileItem)
 	g.POST("/modify", c.fileItemService.ModifyFileItem)
+	g.GET("/preview", c.fileItemService.GetFilePreview)
 	return nil
 }
--- service/file_item/service.go
+++ service/file_item/service.go
@@ -5,8 +5,11 @@ import (
 	"config-management-go/models/file"
 	fileitem "config-management-go/models/file_item"
 	"config-management-go/utils"
+	"fmt"
 	"net/http"
+	"path/filepath"
 	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -29,17 +32,53 @@ type service struct {
 // GetFilePreview implements Service.
 func (s *service) GetFilePreview(c *gin.Context) {
 	idStr := c.Query("fileId")
-	id, err := strconv.Atoi(idStr)
+	fileId, err := strconv.Atoi(idStr)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err})
 		return
 	}
 	var items []fileitem.FieldItem
 	//查询所有文件项目
-	s.fileItemRepo.ListAll(id, &items)
+	s.fileItemRepo.ListAll(fileId, &items)
 	//查询文件内容
 	var file file.File
-	s.fileRepo.ListById(id, &file)
+	s.fileRepo.ListById(fileId, &file)
+	//生成预览文件
+	result := generateFile(items, file)
+	c.JSON(http.StatusOK, utils.NewResponseData(http.StatusOK, "success", result))
+	return
+
+}
+
+//生成预览文件方法
+func generateFile(items []fileitem.FieldItem, file file.File) string {
+	var result strings.Builder
+
+	// 解析文件名后缀
+	fileName := strings.TrimSuffix(file.FileName, filepath.Ext(file.FileName))
+	// 确定注释符号
+	commentSymbol := "#"
+	if filepath.Ext(file.FileName) == ".sql" {
+		commentSymbol = "--"
+	}
+
+	// 遍历每个 FieldItem
+	for _, item := range items {
+		// 拼接 FieldItem 的基本信息
+		content := fmt.Sprintf("%s Created By: %s\n", commentSymbol, item.CreatedBy)
+		content += fmt.Sprintf("%s Created At: %s\n", commentSymbol, item.CreatedAt.Format("2006-01-02 15:04:05"))
+		if !item.UpdatedAt.IsZero() {
+			content += fmt.Sprintf("%s Updated By: %s\n", commentSymbol, item.UpdatedBy)
+			content += fmt.Sprintf("%s Updated At: %s\n", commentSymbol, item.UpdatedAt.Format("2006-01-02 15:04:05"))
+		}
+
+		// 拼接到结果中
+		result.WriteString(fmt.Sprintf("%s------ %s %s Item %d ------\n", commentSymbol, fileName, strings.ToUpper(filepath.Ext(file.FileName)[1:]), item.ID))
+		result.WriteString(content)
+		result.WriteString(fmt.Sprintf("%s------ End of Item ------\n\n", commentSymbol))
+	}
+
+	return result.String()
 }
 
 // ModifyFileItem implements Service.
